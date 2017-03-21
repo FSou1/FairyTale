d233be0b41c9b97f5a39590fbbad7f47fe1a2e1c
fsou1 <my.fsou1@gmail.com>
2017-03-22 00:16:47 +0300

    Update 404 page to make it a little bit frendlier

--- FairyTale/FT.MvcApp/Error/Controllers/ErrorController.cs
+++ FairyTale/FT.MvcApp/Error/Controllers/ErrorController.cs
@@ -8,36 +8,6 @@ namespace FT.MvcApp.Error.Controllers
     public class ErrorController : Controller
     {
         /// <summary>
-        /// Catch all
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Index()
-        {
-            Response.StatusCode = 500;
-
-            var model = new ErrorViewModel {
-                Title = "500. Что-то пошло явно не так"
-            };
-
-            return View(model);
-        }
-
-        /// <summary>
-        /// Catch 401
-        /// </summary>
-        /// <returns></returns>
-        public ActionResult Unauthorized()
-        {
-            Response.StatusCode = 401;
-
-            var model = new ErrorViewModel {
-                Title = "401. Доступ запрещён"
-            };
-
-            return View(model);
-        }
-
-        /// <summary>
         /// Catch 404
         /// </summary>
         /// <returns></returns>
