dfada548a18aa93c99b4bee42b459bd94ee1687c
FSou1 <my.fsou1@gmail.com>
2017-03-21 20:17:41 +0300

    Fix null reference issue

--- FairyTale/FT.MvcApp/Filters/ExceptionHandlerAttribute.cs
+++ FairyTale/FT.MvcApp/Filters/ExceptionHandlerAttribute.cs
@@ -1,11 +1,13 @@
 ﻿using System.Web.Mvc;
 using FT.Components.Serializer;
+using FT.Components.Serializer.Json;
 using log4net;
 
 namespace FT.MvcApp.Filters {
     public class ExceptionHandlerAttribute : HandleErrorAttribute {
         public override void OnException(ExceptionContext filterContext) {
-            var serializer = DependencyResolver.Current.GetService<ISerializer>();
+            var serializer = DependencyResolver.Current.GetService<ISerializer>() 
+                ?? new NewtonsoftJsonSerializer();
 
             var exception = filterContext.Exception;
             var json = serializer.Serialize(exception);
